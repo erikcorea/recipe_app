23a6bf0ac46cc26e00bdf03189407a5bfdd10beb
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-10-07 11:26:40 -0500

    adding category functionality

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
+      <Category />
       <Pages />
     </div>
   );
